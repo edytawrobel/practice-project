0a51f4fcde3c9abe122920485ee7155972430f43
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-03-06 15:18:13 +0000

    operator processes NASA instructions and sends them to rover

--- lib/operator.rb
+++ lib/operator.rb
@@ -6,4 +6,20 @@ class Operator
     @current_plateau = plateau
     @current_rover = rover
   end
+
+  def receive(commands)
+    commands.each_char do |command|
+      case command
+      when "L"
+        current_rover.turn_left
+      when "R"
+        current_rover.turn_right
+      when "M"
+        current_rover.move
+      when 'exit'
+        break
+      end
+    end
+  end
+  
 end
--- spec/operator_spec.rb
+++ spec/operator_spec.rb
@@ -4,9 +4,8 @@ require 'operator'
 describe Operator do
 
     let(:operator) { described_class.new(plateau, rover) }
-    let(:plateau) { Plateau.new(width: 5, height: 5) }
-    let(:rover) { Rover.new(x_coord: 0, y_coord: 0) }
-
+    let(:plateau) { double :plateau, width: 5, height: 5 }
+    let(:rover) { double :rover, x_coord: 0, y_coord: 0, turn_left: nil, turn_right: nil, move: nil }
 
   context "on initialization" do
     it "has some plateau" do
@@ -15,6 +14,24 @@ describe Operator do
 
     it "has a rover" do
       expect(operator.current_rover).to eq rover
+    end
+  end
+
+  context "processing input LRM" do
+
+    it "instructs a rover to turn left" do
+      allow(rover).to receive(:turn_left)
+      operator.receive('L')
+    end
+
+    it "instructs a rover to turn right" do
+      allow(rover).to receive(:turn_right)
+      operator.receive('R')
+    end
+
+    it "instructs a rover to move forward" do
+      allow(rover).to receive(:move)
+      operator.receive('M')
     end
   end
 
