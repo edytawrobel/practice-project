49cf451dc7b4cd746851b8df84ec3aa3da550225
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-03-08 22:31:58 +0000

    refactor operator class, so it is responsible for processing the instructions

--- lib/operator.rb
+++ lib/operator.rb
@@ -3,16 +3,37 @@ class Operator
   attr_reader :current_vehicle, :current_surface
 
   def initialize
-    @current_vehicle ||= nil
     @current_surface ||= nil
+    @current_vehicle ||= nil
+  end
+
+  def get_surface(surface)
+    @current_surface = surface
   end
 
   def deploy(vehicle)
     @current_vehicle = vehicle
   end
 
-  def get_surface(surface)
-    @current_surface = surface
+  def process_input(commands)
+    commands.each_char { |command| send command }
+  end
+
+  def get_position
+    self.current_vehicle.confirm_position
+  end
+
+  private
+
+  def send(command)
+    case command
+      when 'L'
+        @current_vehicle.turn_left
+      when 'R'
+        @current_vehicle.turn_right
+      when 'M'
+        @current_vehicle.move
+    end
   end
 
 end
--- spec/operator_spec.rb
+++ spec/operator_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe Operator do
 
-    let(:operator) { described_class.new }
-    let(:surface) { double :surface, width: 5, height: 5 }
-    let(:vehicle) { double :vehicle, x_coord: 0, y_coord: 0 }
+    let(:operator) { described_class.new() }
+    let(:surface) { double :surface }
+    let(:vehicle) { double :vehicle }
 
   describe '#deploy' do
     it 'can operate a vehicle' do
@@ -17,6 +17,28 @@ describe Operator do
     it 'has a plateau' do
       operator.get_surface(surface)
       expect(operator.current_surface).to eq surface
+    end
+  end
+
+  context "processing input LRM" do
+    before :each do
+      operator.get_surface(surface)
+      operator.deploy(vehicle)
+    end
+
+    it "instructs a rover to turn left" do
+      allow(vehicle).to receive(:turn_left)
+      operator.process_input('L')
+    end
+
+    it "instructs a rover to turn right" do
+      allow(vehicle).to receive(:turn_right)
+      operator.process_input('R')
+    end
+
+    it "instructs a rover to move forward" do
+      allow(vehicle).to receive(:move)
+      operator.process_input('M')
     end
   end
 end
